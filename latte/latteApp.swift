7a4ca2861bb7008891e74e794b352abbb932ff93
arinltte <166289099+arinltte@users.noreply.github.com>
2026-05-24 23:11:43 +0800

    Initial Commit

---
+++ latte/latteApp.swift
@@ -0,0 +1,17 @@
+//
+//  latteApp.swift
+//  latte
+//
+//  Created by Chen Jin Shen on 24/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct latteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
